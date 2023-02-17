2b92dcb7c74cbf261b1aa7ccf793ccf8c054ccae
jadonmj <65021212+jadonmj@users.noreply.github.com>
2023-02-17 11:47:04 +0530

    Add files via upload

---
+++ Frontend Advanced/Week1/Day1/script1.js
@@ -0,0 +1,5 @@
+var eno = 12645;
+var ename = "Scott";
+var job = "Manager";
+var salary = 45000;
+console.log("Empno : " + eno + "," + " Ename  : " +ename + "," + " Job  : " +job + "," + " Salary  : " +salary);
